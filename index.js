b8cb6d5aa63fa52e6237256566eeebcb96d9d05a
Matúš Krajčovič <krajcovic.matus@gmail.com>
2022-10-05 20:09:41 +0200

    placeholder acsii files, responzivita, opravena performace

--- index.js
+++ index.js
@@ -2,7 +2,15 @@ window.onload = () => {
   let title = document.querySelector('#title');
   let text = 'Tím Maruška';
   write(title, text, 0, 100);
+
+  let teamMembers = ['oliver', 'matus', 'katka', 'anett', 'lea', 'andrii', 'michal'];
+  teamMembers.forEach(name => {
+    fetch(`/ascii_obrazky/${name}.txt`).then(res => res.text()).then(data => {
+      document.querySelector(`#img-${name}`).innerHTML = data;
+    });
+  });
 }
+
 function write(element, text, index, speed) {
   if(index == text.length) return;
   let t = element.innerHTML;
