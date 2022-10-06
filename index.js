c2f09aac068c87761dbac6cb0cc4624e1f05f89f
revilo602 <leontiev.oliver@gmail.com>
2022-10-06 18:27:59 +0200

    refactored recursive write into an interval, added blinking

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ window.onload = () => {
   let title = document.querySelector('#title');
   let text = 'Tím Maruška';
   write(title, text, 0, 100);
-
+  //blink(title, 2000)
   let teamMembers = ['lukas','oliver', 'matus', 'katka', 'anett', 'lea', 'andrii', 'michal'];
   teamMembers.forEach(name => {
     fetch(`/ascii_obrazky/${name}.txt`).then(res => res.text()).then(data => {
@@ -12,10 +12,20 @@ window.onload = () => {
 }
 
 function write(element, text, index, speed) {
-  if(index == text.length) return;
-  let t = element.innerHTML;
-  t = t.replace(/.$/, text[index]);
-  t += '_';
-  element.innerHTML = t;
-  setTimeout(() => {write(element, text, index+1, speed)}, speed);
+  let writingTimer = setInterval(() => {
+    let t = element.innerHTML;
+    t = t.replace(/.$/, text[index]);
+    t += '_';
+    element.innerHTML = t;
+    index+=1
+    if(index === text.length) {
+      clearInterval(writingTimer);
+      blink(element, 500)
+    }
+  }, speed);
+}
+function blink(element, speed) {
+  setInterval(() => {
+    element.innerHTML=(element.innerHTML[element.innerHTML.length-1] === "_" ? element.innerHTML.slice(0,-1) : element.innerHTML + "_");
+  }, speed);
 }
